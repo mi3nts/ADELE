43de07111c516fafecf7b68fd2c94b78d19aaeaa
ramijaber3 <61672392+ramijaber3@users.noreply.github.com>
2021-04-25 19:56:15 -0500

    Update CoverPage.py

--- functions/CoverPage.py
+++ functions/CoverPage.py
@@ -1,10 +1,6 @@
 import fitz
 
 
-#THINGS TO ADD
-#DATE, DEVICES, DETAILS, 
-
-
 def createCoverPage():
     
     #SETTING UP
@@ -12,30 +8,50 @@ def createCoverPage():
     page = doc.newPage()
     
     #FORMATTING
-    headerRect = fitz.Rect(70, 200, 500, 700)
-    subHeaderRect = fitz.Rect(70, 260, 500, 700)
-    namesRect = fitz.Rect(70, 400, 500, 700)
+    headerRect = fitz.Rect(70, 160, 500, 700)
+    subHeaderRect = fitz.Rect(70, 330, 500, 700)
+    infoRect = fitz.Rect(70, 420, 500, 700)
+    namesRect = fitz.Rect(70, 600, 500, 700)
+    
+    border = fitz.Rect(10,10,585,830)
+    border2 = fitz.Rect(20,20,575,820)
     
     #TEXT
-    header = "Project ADELE"
+    header = "Automated Documentation to Extract Learning from EEG (ADELE)"
     subHeader = "UTD - MINTS"
-    namesText = "Cristian Garces, Bradley Krakar, Jesse Ladyman, Rami Jaber"
+    infoText = "2020/06/04 T05"
+    namesText = "Cristian Garces, Rami Jaber, Bradley Krakar, Jesse Ladyman"
+    
 
     #TEXTBOXES
     rc = page.insertTextbox(headerRect, header,
-                        align=fitz.TEXT_ALIGN_CENTER , 
-                        fontsize = 40)
+                        align=fitz.TEXT_ALIGN_CENTER, 
+                        fontsize = 35)
 
     rc = page.insertTextbox(subHeaderRect, subHeader,
-                        align=fitz.TEXT_ALIGN_CENTER , 
-                        fontsize = 25)
+                        align=fitz.TEXT_ALIGN_CENTER, 
+                        fontsize = 20)
+        
+    rc = page.insertTextbox(infoRect, infoText,
+                        align=fitz.TEXT_ALIGN_CENTER, 
+                        fontsize = 20)
     
     rc = page.insertTextbox(namesRect, namesText,
-                        align=fitz.TEXT_ALIGN_CENTER , 
+                        align=fitz.TEXT_ALIGN_CENTER, 
                         fontsize = 15)
 
+    #BORDER 
+    shape = page.new_shape()
+    #DRAWING THE RECTANGLES
+    shape.draw_rect(border)
+    shape.draw_rect(border2)
+    #COMMITTING
+    shape.finish()
+    shape.commit()
+
+    
+    
     doc.save("finished.pdf")
 
 #CALLING THE FUNCTION
 createCoverPage()
-
